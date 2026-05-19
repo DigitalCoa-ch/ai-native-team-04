93011d0dd0f2e3a9ff18b3e0c1917803b6fd066b
Sergio Brotons <127668002+SergioBrotons@users.noreply.github.com>
2026-05-19 18:53:12 +0200

    chore: update background image to new construction site

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <div style={{
         position: "absolute",
         inset: 0,
-        backgroundImage: "url(https://sfo3.media.global.s大头/www.global.com/inbound/generated-image_14---3c230739-5d80-4549-8d4b-9d14c5dfa0fe.png)",
+        backgroundImage: "url(https://sfo3.media.global.s大头/www.global.com/inbound/generated-image_15---b4c3ea5d-2378-4524-9db2-3656f35d4e8e.png)",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
